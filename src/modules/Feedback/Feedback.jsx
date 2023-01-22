150bf82b20000f9b2f910ac25c9f384360b81ed6
Vadim-27 <vadim.bulanyi@gmail.com>
2023-01-22 20:47:34 +0200

    one

---
+++ src/modules/Feedback/Feedback.jsx
@@ -0,0 +1,72 @@
+import { Component } from 'react';
+
+import FeedbackBlock from 'modules/FeedbackBlocK/FeedbackBlock';
+import FeedbackVariants from './FeetbackVariants/FeedbackVariants';
+import FeedbackStatistics from './FeedbackStatistics/FeedbackStatistics';
+import Notification from 'shared/components/Notification/Notification';
+
+import css from './feedback.module.css';
+
+const voteOptions = ['good', 'neutral', 'bad'];
+
+class Feedback extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  leaveFeedback = name => {
+    this.setState(prevState => {
+      return {
+        [name]: prevState[name] + 1,
+      };
+    });
+  };
+
+  calcAllTotal() {
+    const { good, neutral, bad } = this.state;
+    const allTotal = good + neutral + bad;
+    return allTotal;
+  }
+
+  calcFeedback(nameProps) {
+    const total = this.calcAllTotal();
+    if (!total) return 0;
+    const value = this.state[nameProps];
+    const resalt = ((value / total) * 100).toFixed(2);
+    return Number(resalt);
+  }
+
+  render() {
+    const allTotal = this.calcAllTotal();
+    const goodTotal = this.state.good;
+
+    const neutralTotal = this.state.neutral;
+    const badTotal = this.state.bad;
+    const positivePercentage = this.calcFeedback('good');
+
+    return (
+      <div className={css.wrapper}>
+        <FeedbackBlock titie="Please">
+          <FeedbackVariants options={voteOptions} leaveFeedback={this.leaveFeedback} />
+        </FeedbackBlock>
+        <FeedbackBlock titie="Statistics">
+          {allTotal ? (
+            <FeedbackStatistics
+              allTotal={allTotal}
+              goodTotal={goodTotal}
+              neutralTotal={neutralTotal}
+              badTotal={badTotal}
+              positivePercentage={positivePercentage}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </FeedbackBlock>
+      </div>
+    );
+  }
+}
+
+export default Feedback;
